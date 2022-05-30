d35a175dfdb6aa9650c3856ebb997707cb0f2989
belle <annabellelyx02@gmail.com>
2022-05-31 01:01:36 +0800

    Updates

---
+++ precache-manifest.3303f98de0d80880d443cea33f3fc6ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e65dec71830c05f18553aa519fbd1a8",
+    "url": "/abc/index.html"
+  },
+  {
+    "revision": "713da9dcfafef2c22ed2",
+    "url": "/abc/static/css/main.a62679fd.chunk.css"
+  },
+  {
+    "revision": "15a2fe9ad89c592aaf6f",
+    "url": "/abc/static/js/2.68978b41.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/abc/static/js/2.68978b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713da9dcfafef2c22ed2",
+    "url": "/abc/static/js/main.404d7e72.chunk.js"
+  },
+  {
+    "revision": "15ccd0abff26d1022846",
+    "url": "/abc/static/js/runtime-main.42ca0cf3.js"
+  }
+]);
